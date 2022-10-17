6e6434c70afaa919a451aa07c4bda0d1dd6b760b
Staci Southerland <stacisoutherland24@gmail.com>
2022-10-17 18:00:39 -0500

    The App Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+//import CodingPhoto from 'C:\Users\Dell-Userx\my-app\src\codingphoto.png'
 
 function App() {
   return (
     <div className="App">
-      Staci
+       <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            Staci Southerland
     </div>
   );
 }
