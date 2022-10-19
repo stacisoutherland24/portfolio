98194d66fb77a59e2f9a864a12c3ffaf64cf9b02
Staci Southerland <stacisoutherland24@gmail.com>
2022-10-19 13:43:32 -0500

    Added Vertical Timeline Experience

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,39 @@
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Staci</h2>
+        <div className="prompt">
+          <p>A software engineer with a passion for creating and learning new things. </p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, NPM, BootStrap
+            </span>
+          </li>
+          
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, C#, C++</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
