67d2610e1c69f31b5a8e68b09b7b2fe40e23512f
Staci Southerland <stacisoutherland24@gmail.com>
2023-01-19 14:42:30 -0600

    setup gh-pages

---
+++ Desktop/portfolio-main/portfolio-main/my-app/src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Staci</h2>
+        <div className="prompt">
+          <p>A software engineer with a passion for creating and learning new things. </p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End Skills</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, NPM, BootStrap
+            </span>
+          </li>
+          
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, C#, C++</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
